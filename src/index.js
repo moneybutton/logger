15216fb2b39fc2288894545bb83163e9d0101ac5
Emilio Almansi <ealmansi@gmail.com>
2018-07-29 23:39:37 -0300

    Added production build for browsers on all isomorphic libraries. (#1399)

--- src/index.js
+++ src/index.js
@@ -1,22 +1,3 @@
-import winston from 'winston'
+import * as log from 'loglevel'
 
-export default winston.createLogger({
-  transports: [
-    new winston.transports.Console({
-      format: winston.format.simple(),
-      level: 'debug',
-      handleExceptions: true,
-      json: false,
-      colorize: true
-    }),
-    new winston.transports.File({
-      level: 'error',
-      filename: 'mb-api-error.log',
-      handleExceptions: true,
-      json: true,
-      maxsize: 5 * 1024 * 1024, // 5MB
-      maxFiles: 5,
-      colorize: false
-    })
-  ]
-})
+export default log
