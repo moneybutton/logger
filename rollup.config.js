b88206fa449c70dfd9abc45f5fe3d2c085eb7abe
Emilio Almansi <ealmansi@gmail.com>
2018-10-03 04:03:43 +0200

    Improve builds: part 2 (#1885)
    * WIP
    
    * WIP
    
    * Revert changes in file
    
    * Update tests.
    
    * Remove MIT licenses.

--- rollup.config.js
+++ rollup.config.js
@@ -1,12 +1,15 @@
 import babel from 'rollup-plugin-babel'
+import builtins from 'rollup-plugin-node-builtins'
+import commonJS from 'rollup-plugin-commonjs'
+import fs from 'fs'
+import globals from 'rollup-plugin-node-globals'
+import minify from 'rollup-plugin-babel-minify'
 import path from 'path'
 import pkg from './package.json'
+import replace from 'rollup-plugin-replace'
+import resolve from 'rollup-plugin-node-resolve'
 
-function isExternal (candidate) {
-  return Object.keys(pkg.dependencies).some(dependency => {
-    return candidate.startsWith(dependency)
-  })
-}
+const PRODUCTION_BUILD = process.env.NODE_ENV === 'production'
 
 export default [
   {
@@ -25,25 +28,104 @@ export default [
       }
     ],
     plugins: [
+      replace(getReplacements()),
       babel({
-        runtimeHelpers: true,
-        exclude: ['node_modules/**'],
         presets: [
           [
             '@babel/preset-env',
             {
               modules: false,
               targets: {
-                node: 'current'
+                node: '8'
               }
             }
           ]
         ],
-        plugins: [
-          '@babel/plugin-external-helpers',
-          '@babel/plugin-transform-runtime'
-        ]
+        plugins: getBabelPlugins()
       })
     ]
-  }
+  },
+  ...(PRODUCTION_BUILD ? [{
+    input: path.resolve(__dirname, 'src', 'index.js'),
+    output: [
+      {
+        file: pkg.unpkg,
+        format: 'iife',
+        name: pkg.library,
+        sourcemap: true
+      }
+    ],
+    context: 'window',
+    plugins: [
+      replace(getReplacements()),
+      globals(),
+      builtins(),
+      resolve({
+        browser: true,
+        preferBuiltins: true
+      }),
+      commonJS({
+        namedExports: {
+          '../../node_modules/loglevel/lib/loglevel.js': [
+            'setLevel',
+            'trace',
+            'debug',
+            'info',
+            'warn',
+            'error'
+          ]
+        }
+      }),
+      babel({
+        exclude: ['../../node_modules/**', 'node_modules/**'],
+        presets: [
+          [
+            '@babel/preset-env',
+            {
+              modules: false,
+              targets: {
+                browsers: ['> 2%']
+              }
+            }
+          ]
+        ],
+        runtimeHelpers: true,
+        plugins: getBabelPlugins({ includeTransformRuntime: true })
+      }),
+      minify({
+        comments: false,
+        banner: getBanner(),
+        bannerNewLine: true
+      })
+    ]
+  }] : [])
 ]
+
+function isExternal (candidate) {
+  return Object.keys(pkg.dependencies).some(dependency => {
+    return candidate.startsWith(dependency)
+  })
+}
+
+function getBanner () {
+  const filePath = path.resolve(__dirname, 'src', 'banner.js')
+  return fs.readFileSync(filePath).toString().trim()
+}
+
+function getBabelPlugins (options = {}) {
+  const plugins = [
+    '@babel/plugin-proposal-object-rest-spread'
+  ]
+  if (options.includeTransformRuntime) {
+    plugins.push('@babel/plugin-transform-runtime')
+  }
+  return plugins
+}
+
+function getReplacements () {
+  const replacements = {}
+  for (const key in process.env) {
+    replacements[`process.env.${key}`] = JSON.stringify(process.env[key])
+  }
+  return replacements
+}
